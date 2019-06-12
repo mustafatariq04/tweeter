e2df4321081b80804684cd813c5b538e6bcad2fe
Mustafa Tariq <mustafatariq04@gmail.com>
2019-06-12 22:11:25 +0000

    updated form validation

--- public/scripts/submit-new-tweet.js
+++ public/scripts/submit-new-tweet.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {    
     $("form").on("submit", function (event) {
-        let $newTweetInput = $("#new-tweet-textarea").serialize();
         event.preventDefault();
-        console.log($newTweetInput);
-        $.ajax({
-            method: "POST",
-            url: "/tweets",
-            data: $newTweetInput
-        });
+        let $newTweetInput = $("#new-tweet-textarea").serialize();
+        let $tweetcounter = $("#new-tweet-textarea").val().length;
+        if ($tweetcounter > 140) {
+            return alert(("Please limit the tweet to 140 characters"));
+        } else if ($tweetcounter === 0 ) {
+            return alert(("Please type in characters to Tweet"));
+        } else {
+            event.preventDefault();
+            console.log($newTweetInput);
+            $.ajax({
+                method: "POST",
+                url: "/tweets",
+                data: $newTweetInput
+            });
+        }    
     });
 });
