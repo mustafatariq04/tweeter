c2e27305e60ce8becd1dd0cbb2dad73638feb53b
Mustafa Tariq <mustafatariq04@gmail.com>
2019-06-12 17:59:06 +0000

    updated functionality to render tweets dynamically

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,38 +4,82 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-    [
-        {
-          "user": {
-            "name": "Newton",
-            "avatars": {
-              "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-              "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-              "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-            },
-            "handle": "@SirIsaac"
-          },
-          "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-          },
-          "created_at": 1461116232227
-        },
-        {
-          "user": {
-            "name": "Descartes",
-            "avatars": {
-              "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-              "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-              "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-            },
-            "handle": "@rd" },
-          "content": {
-            "text": "Je pense , donc je suis"
-          },
-          "created_at": 1461113959088
-        }
-    ] 
-}
+// Test / driver code (temporary). Eventually will get this from the server.
 
-var $tweet = $("<article>").addClass("tweet");
+// Test / driver code (temporary). Eventually will get this from the server.
+$(document).ready(function() { 
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+  
+  function renderTweets(tweets) {  
+    tweets.forEach((element) => {
+      const $tweet = createTweetElement(element);
+      $('#tweets-container').append($tweet);
+    })
+  }
+
+  function createTweetElement (tweet) {
+    const {name, handle} = tweet.user;
+    const avatar = tweet.user.avatars.small;
+    const tweetMessage = tweet.content.text;
+    const dateCreated = tweet.created_at;
+
+    const $tweetArticle = `
+      <article class="tweet">
+      <header>
+        <img class="avatar" src=${avatar}>
+        <h2>${name}</h2>
+        <h5>${handle}</h5>
+      </header>
+      <p>${tweetMessage}</p>
+      <footer>${dateCreated}</footer>
+      `
+    return $tweetArticle;
+  } 
+
+  renderTweets(data);
+});
